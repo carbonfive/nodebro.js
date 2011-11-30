ff9fdaa8044959133348a91b1c27bb45f5da1cff
Alex Cruikshank <alex@carbonfive.com>
2011-11-30 18:44:49 -0500

    updated seed and new version of jade

--- 04-high-scores/scripts/seed.js
+++ 04-high-scores/scripts/seed.js
@@ -6,3 +6,4 @@ query.run('alex',new Date(), 6);
 query.run('rudy', new Date((new Date().getTime() - 3600*32.3243)|0), 4);
 query.run('foo', new Date((new Date().getTime() - 3600*23.293)|0), 11);
 query.run('bar', new Date((new Date().getTime() - 3600*48.2884)|0), 2);
+query.finalize();
