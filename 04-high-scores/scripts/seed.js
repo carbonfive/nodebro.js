4bcdaf57e1139440968c396f82a593767b2305a5
Alex Cruikshank <alex@carbonfive.com>
2012-01-25 13:50:34 -0500

    updated seed script for mongo

--- 04-high-scores/scripts/seed.js
+++ 04-high-scores/scripts/seed.js
@@ -1,9 +1,14 @@
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('highscores');
+var highscores = require('../lib/highscores');
 
-var query = db.prepare('INSERT INTO highscores (nickname,updated,score) VALUES (?,?,?)');
-query.run('alex',new Date(), 6);
-query.run('rudy', new Date((new Date().getTime() - 3600*32.3243)|0), 4);
-query.run('foo', new Date((new Date().getTime() - 3600*23.293)|0), 11);
-query.run('bar', new Date((new Date().getTime() - 3600*48.2884)|0), 2);
-query.finalize();
+function lSequence(list,cb/*(item,next())*/, done/*()*/) {
+  var l = list.slice(0);
+  return (function next() {
+    if ( l.length < 1 ) return done ? done() : null;
+    cb(l.splice(0,1)[0], next);
+  })();
+}
+
+lSequence( ['alex','rudy','foo','bar'], function(name, next) {
+  var range = { alex:[1,2,3,4,5,6], rudy:[1,2,3], foo:[1,2,3,4,5,6,7,8,9,0,1], bar:[1,2] }[name];
+  lSequence(range, function(x,next) { highscores.win(name, next); }, next);
+}, process.exit );
